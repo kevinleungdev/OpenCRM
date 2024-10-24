19a4d8524518dd5244cf89a41d4f5caf39097c11
Keivn Liang <kevindev@Keivns-Mac-mini.local>
2024-10-23 23:56:53 -0700

    Rename project name

--- backend/src/main/java/com/opencrm/app/Application.java
+++ backend/src/main/java/com/opencrm/app/Application.java
@@ -1,13 +1,13 @@
-package app.crm;
+package com.opencrm.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppCrmApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppCrmApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
