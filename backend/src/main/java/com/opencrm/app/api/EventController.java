402bb4cc745100cc6726950b8438dafe1a463c19
kevinleungdev@gmail.com <kevinleungdev@gmail.com>
2024-11-27 19:01:46 -0800

    Enable Java 8 datatime serialization/deserialization

--- backend/src/main/java/com/opencrm/app/api/EventController.java
+++ backend/src/main/java/com/opencrm/app/api/EventController.java
@@ -1,8 +1,26 @@
 package com.opencrm.app.api;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class EventController {
+import com.opencrm.app.api.input.common.OffsetPaging;
+import com.opencrm.app.api.input.common.Sort;
+import com.opencrm.app.api.input.event.EventFilter;
+import com.opencrm.app.model.Event;
 
+import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class EventController {
+    @QueryMapping
+    public Page<Event> events(EventFilter filter, @Argument List<Sort> sorting, @Argument OffsetPaging paging) {
+        log.info("filter: {}, sorters: {}, paging: {}", filter, sorting, paging);
+        return null;
+    }
 }
